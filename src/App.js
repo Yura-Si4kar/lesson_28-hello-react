f03beddff7c1eb283b4fc25c87443c64b927643b
Sichkar Yurii <si4kar2017@gmail.com>
2022-07-07 15:12:07 +0300

    Lesson_28

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      firstNumber: '',
+      secondNumber: '',
+      showResult: 'true',
+    };
+  }
+  render() {
+    return <div>
+      <br />
+      <input name='firstNumber' value={this.state.firstNumber} onChange={this.onFirstInputChange} />
+      <select>
+        <option name='action'>+</option>
+        <option name='action'>-</option>
+        <option name='action'>*</option>
+        <option name='action'>/</option>
+      </select>
+      <input name='secondNumber' value={this.state.secondNumber} onChange={this.onSecondInputChange} />
+      = {this.state.showResult ? <strong>{Number(this.state.firstNumber)+Number(this.state.secondNumber)}</strong> : ''}
+      <br />
+    </div>;
+  }
+
+  onFirstInputChange = (e) => {
+    this.setState({
+      firstNumber: e.target.value,
+    });
+  }
+  
+  onSecondInputChange = (e) => {
+    this.setState({
+      secondNumber: e.target.value,
+    });
+  }
 }
-
-export default App;
